c33c5036442853aca9dae2e4c14e4c08ac9af0e1
gabrielmmart <103005578+gabrielmmart@users.noreply.github.com>
2023-06-29 03:37:52 -0300

    Criação da topbar com modo light e dark funcionando

--- client/src/components/common/PageWrapper.jsx
+++ client/src/components/common/PageWrapper.jsx
@@ -5,6 +5,8 @@ import { setAppState } from "../../redux/features/appStateSlice"
 const PageWrapper = ({state, children}) => {
     const dispatch = useDispatch()
 
+  
+
     useEffect(() => {
       window.scrollTo(0, 0);
       dispatch(setAppState)
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit"
 import userSlice from "./features/userSlice"
-import themeModeSlice from "./features/themesModeSlice";
+import themeModeSlice from "./features/themeModeSlice";
 import appStateSlice from "./features/appStateSlice";
 import globalLoadingSlice from "./features/globalLoadingSlice";
 import authModalSlice from "./features/authModalSlice";
