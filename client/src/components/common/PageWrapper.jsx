d2f6b74d4740c177cf6261a5d1ab4e3b553fae22
gabrielmmart <103005578+gabrielmmart@users.noreply.github.com>
2023-06-29 01:52:07 -0300

    Criação do layout principal e das paginas

---
+++ client/src/components/common/PageWrapper.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { setAppState } from "../../redux/features/appStateSlice"
+
+const PageWrapper = () => {
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+      window.scrollTo(0, 0);
+      dispatch(setAppState)
+    }, [third])
+
+  return (
+    <div>Page/Wrapper</div>
+  )
+}
+
+export default PageWrapper
